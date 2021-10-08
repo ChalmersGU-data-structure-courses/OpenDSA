b0f9f7ef228fb95c2044d2475bb43664825a14e0
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2021-10-08 16:55:31 +0200

    Source code for array stacks

---
+++ SourceCode/Java_Generic/ChalmersGU/DynamicArrayStack.java
@@ -0,0 +1,110 @@
+
+import java.util.Iterator;
+
+/* *** ODSATag: DynamicArrayStack *** */
+/* *** ODSATag: DynamicArrayStackInit *** */
+class DynamicArrayStack<E> implements Stack<E> {
+    private E[] internalArray;
+    private int top;
+
+    @SuppressWarnings("unchecked")
+    public DynamicArrayStack() {
+        internalArray = (E[]) new Object[1];
+        top = 0;
+    }
+/* *** ODSAendTag: DynamicArrayStackInit *** */
+
+/* *** ODSATag: DynamicArrayStackPush *** */
+    public void push(E x) {
+        if (top >= internalArray.length) {
+            resizeArray(2 * internalArray.length);
+        }
+        internalArray[top] = x;
+        top++;
+    }
+/* *** ODSAendTag: DynamicArrayStackPush *** */
+
+/* *** ODSATag: DynamicArrayStackPeek *** */
+    public E peek() {
+        if (!(top > 0)) throw new IndexOutOfBoundsException("peek from empty stack");
+        return internalArray[top-1];
+    }
+/* *** ODSAendTag: DynamicArrayStackPeek *** */
+
+/* *** ODSATag: DynamicArrayStackPop *** */
+    public E pop() {
+        if (!(top > 0)) throw new IndexOutOfBoundsException("pop from empty stack");
+        top--;
+        E x = internalArray[top];
+        internalArray[top] = null;   // For garbage collection
+        if (top <= internalArray.length / 3) {
+            resizeArray(internalArray.length / 2);
+        }
+        return x;
+    }
+/* *** ODSAendTag: DynamicArrayStackPop *** */
+
+/* *** ODSATag: DynamicArrayStackResize *** */
+    private void resizeArray(int newCapacity) {
+        @SuppressWarnings("unchecked")
+        E[] newArray = (E[]) new Object[newCapacity];
+        for (int i = 0; i < top; i++) {
+            newArray[i] = internalArray[i];
+        }
+        internalArray = newArray;
+    }
+/* *** ODSAendTag: DynamicArrayStackResize *** */
+
+    public boolean isEmpty() {
+        return top == 0;
+    }
+
+    public int size() {
+        return top;
+    }
+
+/* *** ODSATag: DynamicArrayStackIterator *** */
+    public Iterator<E> iterator() {
+        return new DynamicArrayStackIterator();
+    }
+
+    private class DynamicArrayStackIterator implements Iterator<E> {
+        private int index;
+        DynamicArrayStackIterator() {
+            index = -1;
+        }
+        public boolean hasNext() {
+            return index + 1 < top;
+        }
+        public E next() {
+            index++;
+            return internalArray[index];
+        }
+    }
+/* *** ODSAendTag: DynamicArrayStackIterator *** */
+/* *** ODSAendTag: DynamicArrayStack *** */
+
+    public void pPrint() {
+        System.out.print(size() + " [ ");
+        for (E e : this) System.out.print(e + " ");
+        System.out.print("| ");
+        for (int i=size(); i<internalArray.length; i++) System.out.print("- ");
+        System.out.println("] " + internalArray.length);
+    }
+
+    public static void main(String[] args) {
+        DynamicArrayStack<String> list = new DynamicArrayStack<>();
+        for (int i=0; i<23; i++) {
+            list.push(String.valueOf((char)(i+65)));
+            if (i % 5 == 0) list.pPrint();
+        }
+        list.pPrint();
+        while (!list.isEmpty()) {
+            if (list.peek() != list.pop()) throw new IndexOutOfBoundsException("ERROR!!!");
+            if (list.size() % 3 == 2) list.pPrint();
+        }
+        list.pPrint();
+    }
+/* *** ODSATag: DynamicArrayStack *** */
+}
+/* *** ODSAendTag: DynamicArrayStack *** */
---
+++ SourceCode/Python/ChalmersGU/DynamicArrayStack.py
@@ -0,0 +1,89 @@
+
+from API import Stack, Iterator
+
+#/* *** ODSATag: DynamicArrayStack *** */
+#/* *** ODSATag: DynamicArrayStackInit *** */
+class DynamicArrayStack(Stack):
+    def __init__(self):
+        self._internalArray = [None]
+        self._top = 0
+#/* *** ODSAendTag: DynamicArrayStackInit *** */
+
+#/* *** ODSATag: DynamicArrayStackPush *** */
+    def push(self, x):
+        if self._top >= len(self._internalArray):
+            self._resizeArray(2 * len(self._internalArray))
+        self._internalArray[self._top] = x
+        self._top += 1
+#/* *** ODSAendTag: DynamicArrayStackPush *** */
+
+#/* *** ODSATag: DynamicArrayStackPeek *** */
+    def peek(self):
+        if not (self._top > 0): raise IndexError("peek from empty stack")
+        return self._internalArray[self._top-1]
+#/* *** ODSAendTag: DynamicArrayStackPeek *** */
+
+#/* *** ODSATag: DynamicArrayStackPop *** */
+    def pop(self):
+        if not (self._top > 0): raise IndexError("pop from empty stack")
+        self._top -= 1
+        x = self._internalArray[self._top]
+        self._internalArray[self._top] = None   # For garbage collection
+        if self._top <= len(self._internalArray) // 3:
+            self._resizeArray(len(self._internalArray) // 2)
+        return x
+#/* *** ODSAendTag: DynamicArrayStackPop *** */
+
+#/* *** ODSATag: DynamicArrayStackResize *** */
+    def _resizeArray(self, newCapacity):
+        newArray = [None] * newCapacity
+        for i in range(self._top):
+            newArray[i] = self._internalArray[i]
+        self._internalArray = newArray
+#/* *** ODSAendTag: DynamicArrayStackResize *** */
+
+    def isEmpty(self):
+        return self._top == 0
+
+    def size(self):
+        return self._top
+
+#/* *** ODSATag: DynamicArrayStackIterator *** */
+    def __iter__(self):
+        return DynamicArrayStackIterator(self._internalArray, self._top)
+
+# Python does not have internal classes, so we have to make the iterator standalone.
+class DynamicArrayStackIterator(Iterator):
+    def __init__(self, array, size):
+        self._array = array
+        self._size = size
+        self._index = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self._index += 1
+        if self._index >= self._size:
+            raise StopIteration
+        return self._array[self._index]
+#/* *** ODSAendTag: DynamicArrayStackIterator *** */
+#/* *** ODSAendTag: DynamicArrayStack *** */
+
+
+def _pprint(l):
+    print(l.size(), "[", " ".join(str(e) for e in l), "|",
+              "- " * (len(l._internalArray) - l.size()) + "]", len(l._internalArray))
+
+if __name__ == '__main__':
+    a = DynamicArrayStack()
+    for i in range(23):
+        a.push(chr(i+65))
+        if a.size() % 5 == 0:
+            _pprint(a)
+    _pprint(a)
+    while not a.isEmpty():
+        assert a.peek() == a.pop(), (a,)
+        if a.size() % 3 == 2:
+            _pprint(a)
+    _pprint(a)
