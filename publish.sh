90afae1eeecc2d1d97230ad7d57bd5f8b03710dc
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2021-10-21 13:30:30 +0200

    Script for copying books and source code to the Published/ directory

---
+++ publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Publish compiled book(s) and source code to the directory Published/
+
+rm -rf Published/*
+
+echo "Copying books:" Books/* "--> Published/"
+cp -r Books/* Published/
+
+shopt -s nullglob
+
+for lang in Pseudo Java Python ; do
+    fromdir=SourceCode/$lang*/ChalmersGU
+    todir=Published/SourceCode/$lang
+    echo "Copying code: $fromdir --> $todir"
+    mkdir -p $todir
+    for fromfile in $fromdir/*.{java,py,txt,md} ; do
+        tofile=$todir/`basename $fromfile`
+	grep -v  '/\* \*\*\* ODSA' $fromfile > $tofile
+    done
+done
+
