feff13c8f4aadbdb5d94495b6fb436019fc0eeaa
Nick Smallbone <nick@smallbone.se>
2021-11-05 17:18:31 +0100

    Add Java implementation of BSTs.

--- SourceCode/Java_Generic/Binary/BST.java
+++ SourceCode/Java_Generic/Binary/BST.java
@@ -1,128 +1,201 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /* *** ODSATag: BST *** */
-// Binary Search Tree implementation
-class BST<E extends Comparable<E>> {
-  private BSTNode<E> root; // Root of the BST
-  private int nodecount; // Number of nodes in the BST
+// A dictionary implemented using a binary search tree.
+public class BST<Key extends Comparable<Key>, Value> implements Iterable<Key> {
+    // A node in a binary search tree.
+    class Node {
+        Key key;
+        Value value;
+        Node left;
+        Node right;
 
-  // constructor
-  BST() { root = null; nodecount = 0; }
-
-  // Reinitialize tree
-  public void clear() { root = null; nodecount = 0; }
-
-  // Insert a record into the tree.
-  // Records can be anything, but they must be Comparable
-  // e: The record to insert.
-  public void insert(E e) {
-    root = inserthelp(root, e);
-    nodecount++;
-  }
-
-  // Remove a record from the tree
-  // key: The key value of record to remove
-  // Returns the record removed, null if there is none.
-  public E remove(E key) {
-    E temp = findhelp(root, key); // First find it
-    if (temp != null) {
-      root = removehelp(root, key); // Now remove it
-      nodecount--;
+        Node(Key key, Value value, Node left, Node right) {
+            this.key = key;
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
     }
-    return temp;
-  }
 
-  // Return the record with key value k, null if none exists
-  // key: The key value to find
-  public E find(E key) { return findhelp(root, key); }
+    // The root of the binary search tree.
+    Node root = null;
 
-  // Return the number of records in the dictionary
-  public int size() { return nodecount; }
+    // Check that the invariant holds.
+    void checkInvariant() {
+        checkInvariantHelper(root, null, null);
+    }
+
+    // Helper method for 'check_invariant'.
+    //
+    // Checks that the node is the root of a valid BST, and that
+    // all keys k satisfy lo < k < hi. The test lo < k is skipped
+    // if lo is None, and k < hi is skipped if hi is None."""
+    void checkInvariantHelper(Node node, Key lo, Key hi) {
+        if (node == null) return;
+
+        if (lo != null && node.key.compareTo(lo) <= 0)
+            throw new AssertionError("key too small");
+
+        if (hi != null && node.key.compareTo(hi) >= 0)
+            throw new AssertionError("key too big");
+
+        // Keys in the left subtree should be < node.key
+        checkInvariantHelper(node.left, lo, node.key);
+        // Keys in the right subtree should be > node.key
+        checkInvariantHelper(node.right, node.key, hi);
+    }
+
+    // Add a key-value pair, or update the value associated with an existing key.
+    public void add(Key key, Value value) {
+        root = addHelper(root, key, value);
+    }
+
+    // Helper method for 'add'.
+    Node addHelper(Node node, Key key, Value value) {
+        if (node == null)
+            return new Node(key, value, null, null);
+
+        else if (key.compareTo(node.key) < 0)
+            node.left = addHelper(node.left, key, value);
+
+        else if (key.compareTo(node.key) > 0)
+            node.right = addHelper(node.right, key, value);
+
+        else
+            node.value = value;
+
+        return node;
+    }
+
+    // Look up a key.
+    public Value get(Key key) {
+        return getHelper(root, key);
+    }
+
+    // Helper method for 'get'.
+    Value getHelper(Node node, Key key) {
+        if (node == null)
+            return null;
+
+        else if (key.compareTo(node.key) < 0)
+            return getHelper(node.left, key);
+
+        else if (key.compareTo(node.key) > 0)
+            return getHelper(node.right, key);
+
+        else
+            return node.value;
+    }
+
+    // Delete a key.
+    public void delete(Key key) {
+        root = deleteHelper(root, key);
+    }
+
+    // Helper method for 'delete'.
+    Node deleteHelper(Node node, Key key) {
+        if (node == null)
+            return null;
+        else if (key.compareTo(node.key) < 0) {
+            node.left = deleteHelper(node.left, key);
+            return node;
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = deleteHelper(node.right, key);
+            return node;
+        } else { // key == node.key
+            if (node.left == null)
+                return node.right;
+            else if (node.right == null)
+                return node.left;
+            else {
+                Node maxNode = maxHelper(node.left);
+                Key maxKey = maxNode.key;
+                Value maxValue = maxNode.value;
+                node.left = deleteHelper(node.left, maxKey);
+                node.key = maxKey;
+                node.value = maxValue;
+                return node;
+            }
+        }
+    }
+
+    // Find the largest key.
+    public Key max() {
+        if (root == null)
+            return null;
+        else
+            return maxHelper(root).key;
+    }
+
+    // Helper method for 'max'.
+    // Returns the node instead, as that's useful in 'deleteHelper'.
+    Node maxHelper(Node node) {
+        // This one is maybe easier to implement non-recursively :)
+        while (node.right != null)
+            node = node.right;
+
+        return node;
+    }
+
+    // Iterate through all keys.
+    // This is called when the user writes 'for (Key key: bst) { ... }.'
+    public Iterator<Key> iterator() {
+        // The easiest way to solve this is to add all keys to an
+        // ArrayList, then iterate through that.
+        ArrayList<Key> keys = new ArrayList<>();
+        iteratorHelper(root, keys);
+        return keys.iterator();
+    }
+
+    // Helper method for 'iterator'
+    void iteratorHelper(Node node, ArrayList<Key> keys) {
+        if (node == null) return;
+        iteratorHelper(node.left, keys);
+        keys.add(node.key);
+        iteratorHelper(node.right, keys);
+    }
+
+    // Override 'toString' to print the contents of the BST.
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        boolean firstKey = true;
+
+        for (Key key: this) {
+            Value value = this.get(key);
+
+            if (!firstKey) str.append(", ");
+            str.append(key.toString() + "->" + value.toString());
+            firstKey = false;
+        }
+
+        return "{" + str + "}";
+    }
+
+    // Some test code to check that the BST is working
+    public static void main(String[] args) {
+        BST<Integer, Integer> bst = new BST<>();
+        int[] keys = {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6};
+        int[] values = new int[keys.length];
+        for (int i = 0; i < values.length; i++) values[i] = i;
+
+        for (int i = 0; i < keys.length; i++) {
+            bst.add(keys[i], values[i]);
+            System.out.println(bst);
+            bst.checkInvariant();
+        }
+
+        for (int i = 0; i < keys.length; i++) {
+            System.out.println(bst.get(keys[i]));
+            bst.checkInvariant();
+        }
+
+        for (int i = 0; i < keys.length; i++) {
+            bst.delete(keys[i]);
+            System.out.println(bst);
+            bst.checkInvariant();
+        }
+    }
+}
 /* *** ODSAendTag: BST *** */
-
-  // Return a record that matches the key value
-/* *** ODSATag: findhelp *** */
-  private E findhelp(BSTNode<E> rt, E key) {
-    if (rt == null) { return null; }
-    if (rt.value().compareTo(key) > 0) {
-      return findhelp(rt.left(), key);
-    }
-    else if (rt.value().compareTo(key) == 0) {
-      return rt.value();
-    }
-    else { return findhelp(rt.right(), key); }
-  }
-/* *** ODSAendTag: findhelp *** */
-
-
-  // Return the current subtree,
-  // modified to contain the new item
-/* *** ODSATag: inserthelp *** */
-  private BSTNode<E> inserthelp(BSTNode<E> rt, E e) {
-    if (rt == null) { return new BSTNode<E>(e); }
-    if (rt.value().compareTo(e) >= 0) {
-      rt.setLeft(inserthelp(rt.left(), e));
-    }
-    else{
-      rt.setRight(inserthelp(rt.right(), e));
-    }
-    return rt;
-  }
-/* *** ODSAendTag: inserthelp *** */
-
-/* *** ODSATag: deletemax *** */
-  // Delete the maximum valued element in a subtree
-  private BSTNode<E> deletemax(BSTNode<E> rt) {
-    if (rt.right() == null) { return rt.left(); }
-    rt.setRight(deletemax(rt.right()));
-    return rt;
-  }
-/* *** ODSAendTag: deletemax *** */
-
-/* *** ODSATag: getmax *** */
-  // Get the maximum valued element in a subtree
-  private BSTNode<E> getmax(BSTNode<E> rt) {
-    if (rt.right() == null) { return rt; }
-    return getmax(rt.right());
-  }
-/* *** ODSAendTag: getmax *** */
-
-  // Remove a node with the key value
-  // Return the tree with the node removed
-/* *** ODSATag: removehelp *** */
-  private BSTNode<E> removehelp(BSTNode<E> rt, E key) {
-    if (rt == null) { return null; }
-    if (rt.value().compareTo(key) > 0) {
-      rt.setLeft(removehelp(rt.left(), key));
-    }
-    else if (rt.value().compareTo(key) < 0) {
-      rt.setRight(removehelp(rt.right(), key));
-    }
-    else { // Found it
-      if (rt.left() == null) { return rt.right(); }
-      else if (rt.right() == null) { return rt.left(); }
-      else { // Two children
-        BSTNode<E> temp = getmax(rt.left());
-        rt.setValue(temp.value());
-        rt.setLeft(deletemax(rt.left()));
-      }
-    }
-    return rt;
-  }
-/* *** ODSAendTag: removehelp *** */
-
-/* *** ODSATag: printhelp *** */
-  private void printhelp(BSTNode<E> rt) {
-    if (rt == null) { return; }
-    printhelp(rt.left());
-    printVisit(rt.value());
-    printhelp(rt.right());
-  }
-/* *** ODSAendTag: printhelp *** */
-
-// Used for testing
-  private void printVisit(E e) { System.out.print(e + " "); }
-
-// Used for testing
-public BSTNode<E> root() {
-  return root;
-}
-}
