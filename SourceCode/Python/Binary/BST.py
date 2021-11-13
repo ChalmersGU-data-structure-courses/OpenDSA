c428b8630fd51ba6cb510f78f1fb27fcdd961aed
Nick Smallbone <nick@smallbone.se>
2021-11-13 14:18:56 +0100

    Fix BSTs to implement course API Python)

--- SourceCode/Python/Binary/BST.py
+++ SourceCode/Python/Binary/BST.py
@@ -85,57 +85,73 @@ class BST:
 # /* *** ODSATag: put *** */
     def put(self, key, value):
         """Add a key-value pair, or update the value associated with
-        an existing key."""
+        an existing key.
 
-        self.root = self.put_helper(self.root, key, value)
+        Returns the value previously associated with the key, or None
+        if the key was not present."""
+
+        self.root, old_value = self.put_helper(self.root, key, value)
+        return old_value
 
     @staticmethod
     def put_helper(node, key, value):
-        """Helper method for 'put'."""
+        """Helper method for 'put'.
+
+        Returns the updated node, and the value previously associated
+        with the key."""
 
         if node is None:
-            return Node(key, value, None, None)
+            return Node(key, value, None, None), None
         elif key < node.key:
-            node.left = BST.put_helper(node.left, key, value)
+            node.left, old_value = BST.put_helper(node.left, key, value)
         elif key > node.key:
-            node.right = BST.put_helper(node.right, key, value)
+            node.right, old_value = BST.put_helper(node.right, key, value)
         else:
+            old_value = node.value
             node.value = value
-        return node
+        return node, old_value
 # /* *** ODSAendTag: put *** */
 
     def remove(self, key):
-        """Delete a key."""
+        """Delete a key.
 
-        self.root = self.remove_helper(self.root, key)
+        Returns the value previously associated with the key, or None
+        if the key was not present."""
+
+        self.root, old_value = self.remove_helper(self.root, key)
+        return old_value
 
     @staticmethod
     def remove_helper(node, key):
-        """Helper method for 'remove'."""
+        """Helper method for 'remove'.
+
+        Returns the updated node, and the value previously associated
+        with the key."""
 
         if node is None:
-            return None
+            return None, None
         elif key < node.key:
-            node.left = BST.remove_helper(node.left, key)
-            return node
+            node.left, old_value = BST.remove_helper(node.left, key)
+            return node, old_value
         elif key > node.key:
-            node.right = BST.remove_helper(node.right, key)
-            return node
+            node.right, old_value = BST.remove_helper(node.right, key)
+            return node, old_value
         else: # key == node.key
             if node.left is None:
-                return node.right
+                return node.right, node.value
             elif node.right is None:
-                return node.left
+                return node.left, node.value
             else:
                 last_node = BST.lastNode(node.left)
                 last_key = last_node.key
                 last_val = last_node.value
                 # We can either use 'deletemax' (as in the next)
                 # or just recursively call removeHelper here
-                node.left = BST.remove_helper(node.left, last_key)
+                node.left, _ = BST.remove_helper(node.left, last_key)
                 node.key = last_key
+                old_value = node.value
                 node.value = last_val
-                return node
+                return node, old_value
 
     def lastKey(self):
         """Find the largest key."""
