3eb62bfa450f920f2a3d986948cda6d64f98f10a
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2021-11-25 07:28:26 +0100

    Layout fixes in Java code

--- SourceCode/Java/BTRecurTutor/RecEffRng.java
+++ SourceCode/Java/BTRecurTutor/RecEffRng.java
@@ -1,12 +1,12 @@
 int range(Node root, int min, int max) {
-  if (root == null)
-    return 0;
-  int result = 0;
-  if (min <= root.key && root.key <= max)
-    result = result + 1;
-  if (min < root.key)
-    result += range(root.left, min, max); 
-  if (root.key < max)
-    result += range(root.right, min, max);
-  return result;
+    if (root == null)
+        return 0;
+    int result = 0;
+    if (min <= root.key && root.key <= max)
+        result += 1;
+    if (min < root.key)
+        result += range(root.left, min, max); 
+    if (root.key < max)
+        result += range(root.right, min, max);
+    return result;
 }
--- SourceCode/Java/BTRecurTutor/RecIneffRng.java
+++ SourceCode/Java/BTRecurTutor/RecIneffRng.java
@@ -1,9 +1,9 @@
- int range(Node root, int min, int max) {
-    if(root == null)
-       return 0;
+int range(Node root, int min, int max) {
+    if (root == null)
+        return 0;
     int result = 0;
     if (min <= root.key && root.key <= max)
-      result = result + 1;
+        result += + 1;
     result += range(root.left, min, max); 
     result += range(root.right, min, max);
     return result;
